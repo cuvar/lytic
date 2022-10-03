539494f9106ecfb7c0edbfeb5310ba0c59c84706
cuvar <luca-mueller@gmx.net>
2022-10-03 13:16:30 +0200

    feat: cors url add https

--- src/pages/api/clicks/[id].ts
+++ src/pages/api/clicks/[id].ts
@@ -10,7 +10,7 @@ export default async function handler(
     res.status(400).json("Invalid request");
     return;
   }
-  res.setHeader("Access-Control-Allow-Origin", "blog.cuvar.dev");
+  res.setHeader("Access-Control-Allow-Origin", "https://blog.cuvar.dev");
   res.setHeader("Vary", "Origin");
 
   if (req.method == "GET") {
