1ef83db7b561cd5ee067b3a2af75969868fda652
cuvar <luca-mueller@gmx.net>
2022-10-07 17:15:41 +0200

    fix: rm nextauth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,36 +2,36 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "@utils/trpc";
 import Stats from "@/components/Stats";
-import { useSession, signIn, signOut } from "next-auth/react";
+// import { useSession, signIn, signOut } from "next-auth/react";
 
 // import * as Components from "umbrella-design";
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <>
-        <Head>
-          <title>Lytic</title>
-          <meta name="description" content="Lytic by cuvar" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div className="flex justify-center items-center text-xl w-screen h-screen">
-          <div className="p-4 mx-auto max-w-screen-md flex flex-col justify-center items-center space-y-4">
-            <h1 className="font-bold text-teal-700 text-3xl">
-              You&apos;re not authenticated
-            </h1>
-            <button
-              onClick={() => signIn()}
-              className="border-2 border-teal-700 rounded-lg py-1 px-3  hover:bg-teal-700 hover:text-white active:bg-teal-900 active:border-teal-900"
-            >
-              Sign in
-            </button>
-          </div>
-        </div>
-      </>
-    );
-  }
+  // const { data: session } = useSession();
+  // if (!session) {
+  //   return (
+  //     <>
+  //       <Head>
+  //         <title>Lytic</title>
+  //         <meta name="description" content="Lytic by cuvar" />
+  //         <link rel="icon" href="/favicon.ico" />
+  //       </Head>
+  //       <div className="flex justify-center items-center text-xl w-screen h-screen">
+  //         <div className="p-4 mx-auto max-w-screen-md flex flex-col justify-center items-center space-y-4">
+  //           <h1 className="font-bold text-teal-700 text-3xl">
+  //             You&apos;re not authenticated
+  //           </h1>
+  //           <button
+  //             onClick={() => signIn()}
+  //             className="border-2 border-teal-700 rounded-lg py-1 px-3  hover:bg-teal-700 hover:text-white active:bg-teal-900 active:border-teal-900"
+  //           >
+  //             Sign in
+  //           </button>
+  //         </div>
+  //       </div>
+  //     </>
+  //   );
+  // }
 
   const { data, isLoading, error } = trpc.useQuery(["lytic.getAll"]);
 
